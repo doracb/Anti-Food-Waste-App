a1130c3cd9bc2c71f6689b71f60aec2110e21afe
Dora Condurache-Bota <doracb2@gmail.com>
2025-12-03 13:49:30 +0200

    added model for Claims table

---
+++ src/models/claim.js
@@ -0,0 +1,30 @@
+const sequelize = require('../sequelize');
+const { DataTypes } = require('sequelize');
+
+const Claim = sequelize.define('Claim', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+    status: {
+        type: DataTypes.ENUM('pending','accepted','rejected'),
+        allowNull: false,
+        defaultValue: 'pending',
+    },
+    food_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+    },
+    claimant_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+    },
+},
+    {
+        tableName: 'Claims',
+        timestamps: true,
+    }
+);
+
+module.exports = Claim;
