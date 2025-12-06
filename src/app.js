bd2fc032c14af874d7c6f39c1f384c2f09acb99d
Dora Condurache-Bota <doracb2@gmail.com>
2025-12-06 14:06:45 +0200

    Correct some typos

--- src/app.js
+++ src/app.js
@@ -9,9 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res => {
-    res.json({ message: "FoodShare API is running..."});
-}));
+app.get("/", (req, res) => {
+    res.json({ message: "API is running..."});
+});
 
 app.use("/api", routes);
 
